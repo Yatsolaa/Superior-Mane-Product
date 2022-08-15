01c94eb18fd3105ee39ee5bf944bd2e7fc14bbd6
Andrii Yatsola <andry.yatsola@gmail.com>
2022-08-16 02:22:00 +0300

    add header and slider-section

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -5,3 +5,44 @@ document.querySelectorAll('.accordion-item__trigger').forEach(function (item) {
     item.parentNode.classList.toggle('accordion-item--active');
   });
 });
+var navigation = document.querySelector('.navigation');
+document.querySelectorAll('.nav-item').forEach(function (item) {
+  return item.addEventListener('click', function () {
+    navigation.classList.toggle('navigation-active');
+  });
+});
+document.querySelector('.burger-menu').addEventListener('click', function () {
+  navigation.classList.toggle('navigation-active');
+});
+$(document).ready(function () {
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav',
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        arrows: true
+      }
+    }]
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true,
+    vertical: true,
+    verticalSwiping: true,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        vertical: false,
+        verticalSwiping: false
+      }
+    }]
+  });
+});
--- script.js
+++ script.js
@@ -3,3 +3,55 @@ document.querySelectorAll('.accordion-item__trigger').forEach((item) =>
         item.parentNode.classList.toggle('accordion-item--active'); 
     })
 );
+
+let navigation = document.querySelector('.navigation')
+
+document.querySelectorAll('.nav-item').forEach((item) => 
+  item.addEventListener('click', () => {
+    navigation.classList.toggle('navigation-active');
+  })
+)
+
+document.querySelector('.burger-menu').addEventListener('click', () => {
+    navigation.classList.toggle('navigation-active');
+})
+    
+$(document).ready(function(){
+  $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider-nav',
+
+      responsive: [
+        {
+          breakpoint: 768,
+            settings: {
+              arrows: true
+            }
+        }
+      ]
+      });
+
+    $('.slider-nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: false,
+      centerMode: false,
+      focusOnSelect: true,
+      vertical: true,
+      verticalSwiping: true,
+
+      responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              vertical: false,
+              verticalSwiping: false,
+            }
+          }
+      ]
+    });
+});
